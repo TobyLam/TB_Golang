a836fe0073cab0d260ce7d8803284a522039e5cd
TobyLam <toby1344319244@163.com>
2022-12-17 18:48:58 +0800

    快速入门

--- src/go_code/study/main/hello.go
+++ src/go_code/study/main/hello.go
@@ -4,5 +4,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello,world!")
+	fmt.Println("hello,world")
 }
---
+++ src/go_code/study/main/hi.go
@@ -0,0 +1,10 @@
+// 课堂联系,输出hi,world
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hi,world")
+	fmt.Println("hi,world")
+	fmt.Println("hi,world")
+}
