9e46e956856d24e4eec073432243d58eae81dcc9
TobyLam <toby1344319244@163.com>
2022-12-17 20:14:18 +0800

    转义字符

---
+++ src/go_code/escaptechar/exec01.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	//练习
+	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京")
+}
--- src/go_code/escaptechar/main.go
+++ src/go_code/escaptechar/main.go
@@ -7,4 +7,12 @@ func main() {
 	fmt.Println("tom\tjack")
 	//\n
 	fmt.Println("hello\nworld")
+	//\\
+	fmt.Println("C:\\Users\\TobyLam\\Desktop\\GolangNote")
+	//\"
+	fmt.Println("tom说:\"i love you\"")
+	//\r 回车，从当前行最开始前面开始输出，覆盖掉以前的内容
+	fmt.Println("hello,\rworld")
+	fmt.Println("天龙八部雪山飞狐\rworld")
+
 }
