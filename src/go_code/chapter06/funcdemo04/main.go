5238e57ed3fbd947c403e4fba3d57c0d38f1dbd9
TobyLam <toby1344319244@163.com>
2023-11-26 22:21:30 +0800

    feat:函数注意事项

---
+++ src/go_code/chapter06/funcdemo04/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 函数中的变量是局部的，函数外不生效
+func test() {
+	//n1是 test函数的局部变量，只能在test函数中使用
+	//var n1 int = 10
+}
+
+func test02(n1 int) {
+	n1 = n1 + 10
+	fmt.Println("test02() n1=", n1)
+}
+
+//不支持传统的函数重载
+//func test02(n1 int, n2 int) {
+//
+//}
+
+// n1 就是 *int 类型
+func test03(n1 *int) {
+	fmt.Printf("n1的地址= %v\n", &n1)
+	*n1 = *n1 + 10
+	fmt.Println("test03() n1=", *n1)
+}
+
+func main() {
+	//这里不能使用n1,因为n1是test函数的局部变量
+	//fmt.Println("n1=", n1)
+
+	//num := 20
+	//test02(num)
+	//fmt.Println("main() num= ", num)
+
+	num := 20
+	fmt.Printf("num的地址=%v\n", &num)
+	test03(&num)
+	fmt.Println("main() num=", num)
+
+}
