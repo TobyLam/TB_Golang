44ac3f92be22bf6473b26b6e17c9c0dde64a8666
TobyLam <toby1344319244@163.com>
2024-01-07 12:06:56 +0800

    feat:闭包实践

--- src/go_code/chapter06/funcdemo07/main.go
+++ src/go_code/chapter06/funcdemo07/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // 累加器
@@ -17,6 +18,37 @@ func AddUpper() func(int) int {
 	}
 }
 
+/*
+1) 编写一个函数 makeSuffix(suffix string) 可以接收一个文件后缀名(比如.jpg)，并返回一个闭包
+2) 调用闭包，可以传入一个文件名，如果该文件名没有指定的后缀(比如.jpg) ,则返回 文件名.jpg , 如
+果已经有.jpg 后缀，则返回原文件名。
+3) 要求使用闭包的方式完成
+4) strings.HasSuffix , 该函数可以判断某个字符串是否有指定的后缀。
+*/
+func makeSuffix(suffix string) func(string) string {
+
+	// 返回的func和外部变量suffix形成闭包
+	return func(name string) string {
+		//如果name 没有指定后缀，则加上，否则就返回原来的名字
+		if strings.HasSuffix(name, suffix) == false {
+			return name + suffix
+		}
+
+		return name
+	}
+}
+
+func makeSuffix2(suffix string, name string) string {
+
+	//如果name 没有指定后缀，则加上，否则就返回原来的名字
+	if strings.HasSuffix(name, suffix) == false {
+		return name + suffix
+	}
+
+	return name
+
+}
+
 func main() {
 	//使用前面的代码
 	f := AddUpper()
@@ -24,4 +56,12 @@ func main() {
 	fmt.Println(f(2)) //13
 	fmt.Println(f(3)) //16
 
+	// 测试makeSuffix 的使用
+	// 返回一个闭包
+	f2 := makeSuffix(".jpg") //如果使用闭包完成，好处是只需要传入一次后缀
+	fmt.Println("文件名处理后=", f2("winter"))
+	fmt.Println("文件名处理后=", f2("bird.jpg"))
+
+	fmt.Println("文件名处理后=", makeSuffix2("jpg", "winter"))
+	fmt.Println("文件名处理后=", makeSuffix2("jpg", "bird.jpg"))
 }
