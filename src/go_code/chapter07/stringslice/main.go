d04f467d90f0e9e0d6ed2220095d09bc5d187273
TobyLam <toby1344319244@163.com>
2024-05-04 16:07:50 +0800

    feat:slice

--- src/go_code/chapter07/stringslice/main.go
+++ src/go_code/chapter07/stringslice/main.go
@@ -13,6 +13,22 @@ func main() {
 
 	fmt.Println("slice=", slice)
 
-	var d[];
+	//string是不可变的，也就是不能通过str[0] = ”z“ 方式来修改字符串
+	//str[0] = 'z'  //编译不通过，报错，原因：string不可变
+
+	//如果需要修改字符串，可以先将string -> []byte / 或者 []rune ->修改 -> 重新转成string
+	// "hello@TobyLam” -> "zello@TobyLam"
+	//arr1 := []byte(str)
+	//arr1[0] = 'z'
+	//str = string(arr1)
+	//fmt.Println("str=", str)
+
+	// 转成 []byte后，可以处理英文和数字,不能处理中文
+	// 原因是 []byte 字节来处理，而一个汉字，，是3个字节，因此出现乱码
+	// 解决方法： 将string 转成 []rune 即可， 因为 []rune是按字符处理，兼容汉字
+	arr1 := []rune(str)
+	arr1[0] = '广'
+	str = string(arr1)
+	fmt.Println("str=", str)
 
 }
