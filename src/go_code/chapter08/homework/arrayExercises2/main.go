c316f9c05641fc019c8313178362578de128bb03
TobyLam <toby1344319244@163.com>
2024-05-14 18:20:06 +0800

    feat:数组练习2

---
+++ src/go_code/chapter08/homework/arrayExercises2/main.go
@@ -0,0 +1,50 @@
+package main
+import(
+    "fmt"
+    "math/rand"
+    "time"
+    "math"
+)
+
+//插入数据并排序
+func insertAndSort(slice []float64,num float64)[]float64{
+     slice = append(slice,num)
+     //冒泡排序
+     for i:= 0;i < len(slice)-1; i++ {
+         for j:= 0; j < len(slice) - 1 - i; j++ {
+            if slice[j] > slice[j+1]{
+               slice[j],slice[j+1] = slice[j+1],slice[j]
+            }
+         }
+     }
+     return slice
+}
+
+func main(){
+  /**
+  已知有个排序好（升序）的数组，要求插入一个元素，最后打印该数组，顺序依然是升序
+  */
+  max := 100.0
+  min := 0.0
+  precision := 2
+  
+  var arr [10]float64
+  rand.Seed(time.Now().Unix())
+
+  for i := 0; i < len(arr);i++{
+      arr[i] = math.Floor(rand.Float64()*(max-min)*math.Pow10(precision)+min*math.Pow10(precision)) / math.Pow10(precision)
+  }
+
+  fmt.Printf("原数组=%v\n",arr)
+
+  num := 0.0
+  fmt.Println("请输入需要插入的数字:")
+  fmt.Scanln(&num)
+  
+  //声明一个切片
+  var slice = arr[:]
+  //插入元素并排序
+  newSlice := insertAndSort(slice,num)
+  fmt.Printf("插入元素后的结果：%v\n",newSlice)  
+}
+
