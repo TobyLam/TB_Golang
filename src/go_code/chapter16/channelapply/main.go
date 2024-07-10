ee4a53919c15fedcb90f9512ddb26197601d4ca0
TobyLam <toby1344319244@163.com>
2024-07-10 16:23:38 +0800

    feat:channel应用，配合goroutine进行读写

---
+++ src/go_code/chapter16/channelapply/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	_ "time"
+)
+
+// write Data
+func writeData(intChan chan int) {
+	for i := 1; i <= 50; i++ {
+		//放入数据
+		intChan <- i
+		fmt.Println("writeData ", i)
+		//time.Sleep(time.Second)
+	}
+	close(intChan) //关闭
+}
+
+// read data
+func readData(intChan chan int, exitChan chan bool) {
+
+	for {
+		v, ok := <-intChan
+		if !ok {
+			break
+		}
+		//time.Sleep(time.Second)
+		fmt.Printf("readData 读到数据=%v\n", v)
+	}
+	//readData 读取完数据后，即任务完成
+	exitChan <- true //多余，关闭channel，v,ok：=<-exitChan  ，ok就是false了
+	close(exitChan)
+}
+
+func main() {
+
+	//创建两个管道
+	intChan := make(chan int, 50)
+	exitChan := make(chan bool, 1)
+
+	go writeData(intChan)
+	go readData(intChan, exitChan)
+
+	//time.Sleep(time.Second * 10)
+	for {
+		_, ok := <-exitChan //实际判断的是exitChan有没有被关闭。。。
+		if !ok {
+			break
+		}
+	}
+
+}
