3b11f44e3eb48faf564d0a49b6da7b59ea4afbad
TobyLam <toby1344319244@163.com>
2024-07-15 10:41:42 +0800

    feat:协程异常处理

---
+++ src/go_code/chapter16/channeldetails/demo03/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 函数
+func sayHello() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second)
+		fmt.Println("hello,world")
+	}
+}
+
+// 函数
+func test() {
+	//使用defer + recover
+	defer func() {
+		//捕获test抛出的panic
+		if err := recover(); err != nil {
+			fmt.Println("test() 发生错误")
+		}
+	}()
+	//定义一个map
+	var myMap map[int]string
+	myMap[0] = "golang" //error
+}
+
+func main() {
+	go sayHello()
+	go test()
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("main() ok=", i)
+		time.Sleep(time.Second)
+	}
+}
