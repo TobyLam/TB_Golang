ad78e4aa3a74d049cd5583129cd0ee8f924c9026
TobyLam <toby1344319244@163.com>
2024-09-25 21:13:47 +0800

    feat:GoWeb 处理静态资源

---
+++ src/bookstore001/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// IndexHandler 去首页
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	//解析模板
+	t := template.Must(template.ParseFiles("views/index.html"))
+	//执行
+	t.Execute(w, "")
+}
+
+func main() {
+	//设置处理静态资源,如css和js
+	// http.Dir()使用绝对路径views/static，而非/views/static
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("views/static"))))
+	//直接去html页面
+	http.Handle("/pages/", http.StripPrefix("/pages/", http.FileServer(http.Dir("views/pages"))))
+
+	http.HandleFunc("/main", IndexHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
