5fba81098937b495f3ee8f7e9097c64ce319bc61
TobyLam <toby1344319244@163.com>
2024-09-27 01:01:58 +0800

    feat:GoWeb 模板,条件动作和迭代动作

---
+++ src/web03_actions/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"time"
+	"web03_actions/model"
+)
+
+func testIf(w http.ResponseWriter, r *http.Request) {
+	//解析模板文件
+	t := template.Must(template.ParseFiles("if.html"))
+	age := 17
+	//执行
+	t.Execute(w, age > 18)
+}
+
+func testRange(w http.ResponseWriter, r *http.Request) {
+	//解析模板文件
+	t := template.Must(template.ParseFiles("range.html"))
+	var emps []*model.Employee
+
+	emp := &model.Employee{
+		ID:       1,
+		LastName: "宋江",
+		Email:    "jsy@ls.com",
+	}
+	emps = append(emps, emp)
+	emp = &model.Employee{
+		ID:       2,
+		LastName: "卢俊义",
+		Email:    "ljy@ls.com",
+	}
+	emps = append(emps, emp)
+	//执行
+	t.Execute(w, emps)
+}
+
+func testRangeMap(w http.ResponseWriter, r *http.Request) {
+	//解析模板文件
+	t := template.Must(template.ParseFiles("rangemap.html"))
+	var emps map[string]string
+	emps = make(map[string]string)
+	emps["神行太保"] = "戴宗"
+	emps["浪子"] = "燕青"
+	emps["赤发鬼"] = "刘唐"
+	//执行
+	t.Execute(w, emps)
+}
+
+func formatDate(t time.Time) string {
+	format := "2006年01月02日"
+	return t.Format(format)
+}
+
+func actionfunc(w http.ResponseWriter, r *http.Request) {
+	funcMap := template.FuncMap{ // 定义映射
+		"chndate": formatDate,
+	}
+	t := template.New("rangechannel.html").Funcs(funcMap) // 绑定映射到模板
+	t, _ = t.ParseFiles("rangechannel.html")
+	t.Execute(w, time.Now())
+}
+
+func main() {
+	http.HandleFunc("/testIf", testIf)
+	http.HandleFunc("/testRange", testRange)
+	http.HandleFunc("/testRangeMap", testRangeMap)
+	http.HandleFunc("/testRangeChannel", actionfunc)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ src/web03_actions/model/employee.go
@@ -0,0 +1,7 @@
+package model
+
+type Employee struct {
+	ID       int
+	LastName string
+	Email    string
+}
